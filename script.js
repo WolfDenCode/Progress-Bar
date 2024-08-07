aff19628ec2ebbd0b18d1d222120099f83bc2ee7
“WolfDen <wolfcodeden@gmail.com>
2024-08-07 17:51:48 +0200

    Style changes

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let currentActive = 1;
+let currentlyActive = 0;
 
 const circles = document.querySelectorAll(".circle");
 const bar = document.getElementById("bar");
@@ -6,31 +6,29 @@ const prev = document.getElementById("prev");
 const next = document.getElementById("next");
 
 function updateBarWidth() {
-    const completes = document.querySelectorAll(".complete");
-    const barWidth = (completes.length) / (circles.length - 1) * 100 + "%";
-    bar.style.width = barWidth;
+  const completes = document.querySelectorAll(".complete");
+  const barWidth = (completes.length / (circles.length - 1)) * 100 + "%";
+  bar.style.width = barWidth;
 }
 
-next.addEventListener("click", function() {
-    if (currentActive < circles.length) {
-        circles[currentActive - 1].classList.remove("active");
-        circles[currentActive - 1].classList.add("complete");
-        currentActive += 1;
-        circles[currentActive - 1].classList.add("active");
-        updateBarWidth();
-        console.log(currentActive);
-    }
+next.addEventListener("click", function () {
+  if (currentlyActive < circles.length - 1) {
+    circles[currentlyActive].classList.remove("active");
+    circles[currentlyActive].classList.add("complete");
+    currentlyActive++;
+    circles[currentlyActive].classList.add("active");
+    updateBarWidth();
+  }
 });
 
-prev.addEventListener("click", function() {
-    if (currentActive > 1) {
-        circles[currentActive - 1].classList.remove("active");
-        circles[currentActive - 2].classList.remove("complete");
-        currentActive -= 1;
-        circles[currentActive - 1].classList.add("active");
-        updateBarWidth();
-        console.log(currentActive);
-    }
+prev.addEventListener("click", function () {
+  if (currentlyActive > 0) {
+    circles[currentlyActive].classList.remove("active");
+    circles[currentlyActive - 1].classList.remove("complete");
+    currentlyActive--;
+    circles[currentlyActive].classList.add("active");
+    updateBarWidth();
+  }
 });
 
 updateBarWidth();
